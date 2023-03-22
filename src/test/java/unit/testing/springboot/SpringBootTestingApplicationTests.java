32ba478c7dd82d71e5615a7b775bdd16289e00e6
sonu <karthik@gmail.com>
2023-03-22 22:06:20 +0530

    my first commit

---
+++ src/test/java/unit/testing/springboot/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package unit.testing.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
